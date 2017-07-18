0944c164ee3739c7f76611765e333cbf079eaef2
Samir Gossain <samir.gossain@gmail.com>
2017-07-18 15:17:20 +0100

    Search terms entered are returned on the next page

--- giftrip.rb
+++ giftrip.rb
@@ -1,18 +1,19 @@
-
 require 'sinatra/base'
 
 class GifTrip < Sinatra::Base
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-post '/mantra' do
-  "mantra"
-end
+  post '/mantra' do
+    @mantra = params[:mantra]
+    erb :trippin
+  end
 
-# start the server if ruby file executed directly
-  run! if app_file == $0
+  # start the server if ruby file executed directly
+    run! if app_file == $0
+
 end
 
 
